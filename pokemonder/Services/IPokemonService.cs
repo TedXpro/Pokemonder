79563294a54dcc9fdb16f7fd0f8864f50735fb4b
TedXpro <johannes.woldeyes@gmail.com>
2025-01-06 15:34:11 +0300

    remove unnecessary code

--- pokemonder/Services/IPokemonService.cs
+++ pokemonder/Services/IPokemonService.cs
@@ -1,15 +1,3 @@
-// using pokemonder.Models;
-
-// public interface IPokemonService
-// {
-//     public Task<List<Pokemon>> GetPokemons();
-//     public Task<Pokemon> GetPokemon(string id);
-//     public Task<Pokemon> AddPokemon(Pokemon pokemon);
-//     public Task<bool> UpdatePokemon(string id, Pokemon pokemon);
-//     public Task<bool> DeletePokemon(string id);
-//     public Task<Pokemon> GetPokemonByName(string name);
-// }
-
 using pokemonder.Models;
 
 namespace pokemonder.Services
