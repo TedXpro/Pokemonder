78df99950b3a24d8aea91d50656c722026a478c2
TedXpro <johannes.woldeyes@gmail.com>
2025-01-08 19:53:42 +0300

    made the methods inside the interface public

--- pokemonder/Services/IPokemonService.cs
+++ pokemonder/Services/IPokemonService.cs
@@ -4,11 +4,11 @@ namespace pokemonder.Services
 {
     public interface IPokemonService
     {
-        Task<List<Pokemon>> GetPokemons();
-        Task<Pokemon> GetPokemon(string id);
-        Task<bool> AddPokemon(Pokemon pokemon);
-        Task<bool> UpdatePokemon(string id, Pokemon pokemon);
-        Task<bool> DeletePokemon(string id);
-        Task<Pokemon> GetPokemonByName(string name);
+        public Task<List<Pokemon>> GetPokemons();
+        public Task<Pokemon> GetPokemon(string id);
+        public Task<bool> AddPokemon(Pokemon pokemon);
+        public Task<bool> UpdatePokemon(string id, Pokemon pokemon);
+        public Task<bool> DeletePokemon(string id);
+        public Task<Pokemon> GetPokemonByName(string name);
     }
 }
--- pokemonder/obj/Debug/net8.0/pokemonder.AssemblyInfo.cs
+++ pokemonder/obj/Debug/net8.0/pokemonder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemonder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79563294a54dcc9fdb16f7fd0f8864f50735fb4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42760389a6b6cecf2e13f60b8cf3c066d516226c")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemonder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemonder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
