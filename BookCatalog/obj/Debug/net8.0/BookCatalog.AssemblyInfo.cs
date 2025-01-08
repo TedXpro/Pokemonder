e50b291dfbe35b58c6ccad12f4705d5c9cfbf834
TedXpro <johannes.woldeyes@gmail.com>
2025-01-08 19:56:13 +0300

    made some few changes inside the obj folder

--- BookCatalog/obj/Debug/net8.0/BookCatalog.AssemblyInfo.cs
+++ BookCatalog/obj/Debug/net8.0/BookCatalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookCatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79563294a54dcc9fdb16f7fd0f8864f50735fb4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42760389a6b6cecf2e13f60b8cf3c066d516226c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookCatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookCatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
